d4fe510342bc24258dca878f9c2aba4cbd44d9ce
St-andrey <40577453+St-andrey@users.noreply.github.com>
2020-03-05 20:55:55 +0300

    +ssubscribe section

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,13 @@
 
 $(function () {
-    $('.about-video-box').on('click', '.about-video-additional__poster', function () {
-        var src = $(this).attr('src');
-        $(this).parent().parent().parent().find('.about-video-main .about-video-main__poster').attr('src', src);
-    });
-
-    $('.reviews-slider').slick({
-        slidesToShow: 2,
-        slidesToScroll: 2,
+   
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
         dots: true,
         arrows: false,
+        vertical: true,
     
     });
-    
-    $('.gallery-slider').slick({
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        dots: false,
-        arrows: true,
-    });
-    
+            
 });
