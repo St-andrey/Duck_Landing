904f38e7dd7beba9dc005276c94d9ed56d8cd6c0
St-andrey <40577453+St-andrey@users.noreply.github.com>
2020-03-19 18:55:05 +0300

    +parallax

--- app/js/main.js
+++ app/js/main.js
@@ -3,11 +3,23 @@ $(document).ready(function () {
 
     var controller = new ScrollMagic.Controller;
 
+    //parallax section animated
+    var parallaxDuckling = new ScrollMagic.Scene({
+        triggerElement: '.parallax-wrapper',
+        triggerHook: 1,
+        duration: '200%'
+    })
+        .setTween(TweenMax.from('.parallax-bg', 1, {y: '-35%', ease:Power0.easeNone}))
+        .addTo(controller);
+
+    //subsribe section animated
     var fixedHeaderBar = new ScrollMagic.Scene({
         triggerElement: '#subscribe'
     })
         .setClassToggle('#subscribe', 'fade-in')
         .addTo(controller);
+
+
 
 
     $('.slider').slick({
