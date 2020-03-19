73de6438251c27ca6fdbe061378a2ca596ba5976
St-andrey <40577453+St-andrey@users.noreply.github.com>
2020-03-19 16:14:51 +0300

    + very first scrollmagic animation

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,22 @@
 
-$(function () {
-   
+$(document).ready(function () {
+
+    var controller = new ScrollMagic.Controller;
+
+    var fixedHeaderBar = new ScrollMagic.Scene({
+        triggerElement: '#subscribe'
+    })
+        .setClassToggle('#subscribe', 'fade-in')
+        .addTo(controller);
+
+
     $('.slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         dots: true,
         arrows: false,
         vertical: true,
-    
+
     });
-            
+
 });
